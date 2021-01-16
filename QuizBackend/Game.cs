fc3c139ad07d997db1e0f765e3ad5ca9c8cb3bed
3MinutesProgramming <threeminutesprogramming@gmail.com>
2021-01-16 08:11:17 +0100

    Add first classes

---
+++ QuizBackend/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBackend
+{
+    public class Game
+    {
+        public Question GetQuestion()
+        {
+            Question question = new Question();
+            question.Text = "What was the name of brilliant scientist Einstein?";
+            question.Answer1 = "Albert";
+            question.Answer2 = "Anthony";
+            question.Answer3 = "Aaron";
+            question.Answer4 = "Andrew";
+            return question;
+        }
+    }
+}
